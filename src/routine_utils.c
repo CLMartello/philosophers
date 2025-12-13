9adc1e8f02e53a2ce95859171e3112a4966e40fa
CLMartello <carolinamartello1@gmail.com>
2025-12-13 20:00:40 +0000

    feat: condition to only one philosopher

--- src/routine_utils.c
+++ src/routine_utils.c
@@ -6,7 +6,7 @@
 /*   By: clumertz <clumertz@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 14:49:38 by clumertz          #+#    #+#             */
-/*   Updated: 2025/12/10 15:44:37 by clumertz         ###   ########.fr       */
+/*   Updated: 2025/12/13 14:02:49 by clumertz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,16 +24,14 @@ long	get_time(t_table *table)
 
 long	get_timestamp(t_table *table)
 {
-	long	start_time;
 	long	timestamp;
 
-	start_time = (table->start_time.tv_sec * 1000)
-		+ (table->start_time.tv_usec / 1000);
-	timestamp = get_time(table) - start_time;
+	timestamp = get_time(table) - ((table->start_time.tv_sec * 1000)
+			+ (table->start_time.tv_usec / 1000));
 	return (timestamp);
 }
 
-int	decide_first_fork(t_philo *philo)
+int	decide_first_fork(t_philo *philo) //min
 {
 	if (philo->index % 2 == 0)
 		return (philo->left);
@@ -41,10 +39,21 @@ int	decide_first_fork(t_philo *philo)
 		return (philo->right);
 }
 
-int	decide_second_fork(t_philo *philo)
+int	decide_second_fork(t_philo *philo) //max
 {
 	if (philo->index % 2 == 0)
 		return (philo->right);
 	else
 		return (philo->left);
+}
+
+void	only_one_philo(t_table *table)
+{
+	print_mutex(table, &table->philos[0], get_timestamp(table),
+		"has taken a fork\n");
+	usleep(table->time_to_die * 1000);
+	print_mutex(table, &table->philos[0], get_timestamp(table), "died\n");
+	pthread_mutex_lock(&table->dead);
+	table->dead_flag = 1;
+	pthread_mutex_unlock(&table->dead);
 }
